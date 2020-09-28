ee2e5faebdc52d5a3a4303b3f130fe03d204839f
Enrique <enriquecardenas96@gmail.com>
2020-09-27 19:03:56 -0700

    Change class components to function components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import NavBar from "./components/navbar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar />
-//       <div className="container">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     </div>
-//   );
-// }
+const App = () => {
+  const [ page, setPage]= React.useState(1);
 
-class App extends Component {
-  state = {
-    page: 1,
-  };
 
-  message = () => {
-    console.log("Hello World!");
-    this.setState({ page: 1 });
+  /* Function declaration
+  currentPage(){
+  }
+  */
 
-    console.log("Page: ", this.state.page);
-  };
+  // function expression
+  const currentPage = () => {
+    switch (page) {
+      case 0:
+        return <h1>Admin Page</h1>;
+      case 1:
+        return homePage();
+      case 2:
+        return aboutPage();
+      case 3:
+        return <h1>Third Page</h1>;
+      default:
+        return <h1>First Page</h1>;
+    }
+  }
 
-  render() {
-    return (
+  const homePage = () => {
+    console.log("Welcome to our home page");
+
+    return <div className="container">Welcome to our home page</div>;
+  }
+
+  const aboutPage = () => {
+    return <div className="container">We are currently building up</div>;
+  }
+
+
+   return (
       <div className="App">
         <NavBar
-          onHome={this.homePage}
-          page={this.state.page}
-          setPage={this.setPage}
+          onHome={homePage}
+          page={page}
+          setPage={setPage}
         />
 
-        <div className="container">{this.currentPage()}</div>
+        <div className="container">{currentPage()}</div>
         <div className="container">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -70,61 +68,7 @@ class App extends Component {
           </header>
         </div>
       </div>
-    );
-  }
-
-  // currentPage() {
-  //   return this.state.page;
-  // }
-
-  setPage = (page) => {
-    console.log("Setting page --> ", page);
-    this.setState({ page: page });
-  };
-
-  homePage() {
-    console.log("Welcome to our home page");
-
-    return <div className="container">Welcome to our home page</div>;
-  }
-
-  aboutPage() {
-    return <div className="container">We are currently building up</div>;
-  }
-
-  // aboutPage = () => {
-  //   console.log("Our about us page");
-  //   this.setState({ page: 1 });
-  // };
-
-  currentPage() {
-    switch (this.state.page) {
-      case 0:
-        return <h1>Admin Page</h1>;
-        break;
-      case 1:
-        return this.homePage();
-        break;
-      case 2:
-        return this.aboutPage();
-        break;
-      case 3:
-        return <h1>Third Page</h1>;
-        break;
-      default:
-        return <h1>First Page</h1>;
-    }
-  }
-
-  // homePage() {
-  //   console.log("Printing out the home page");
-  //   var homePageValue = 1;
-  //   this.setState({ page: homePageValue });
-  // }
-
-  printOutPage() {
-    console.log("Printing out the page");
-  }
+   );
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,58 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-// const NavBar = (onHome) => {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <a className="navbar-brand" href="#">
-//         Navbar
-//       </a>
-//       <button
-//         className="navbar-toggler"
-//         type="button"
-//         data-toggle="collapse"
-//         data-target="#navbarNav"
-//         aria-controls="navbarNav"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-//       <div className="collapse navbar-collapse" id="navbarNav">
-//         <ul className="navbar-nav">
-//           <li className="nav-item active">
-//             <a className="nav-link" onClick={onHome} href="#">
-//               Home
-//             </a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">
-//               Features
-//             </a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">
-//               Pricing
-//             </a>
-//           </li>
-//           <li className="nav-item">
-//             <a
-//               className="nav-link disabled"
-//               href="#"
-//               tabindex="-1"
-//               aria-disabled="true"
-//             >
-//               Disabled
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-
-class NavBar extends Component {
-  render() {
-    return (
+const NavBar = (props) => {
+   return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar
@@ -74,7 +23,7 @@ class NavBar extends Component {
             <li className="nav-item active">
               <a
                 className="nav-link"
-                onClick={() => this.props.setPage(1)}
+                onClick={() => props.setPage(1)}
                 href="#"
               >
                 Home
@@ -83,7 +32,7 @@ class NavBar extends Component {
             <li className="nav-item">
               <a
                 className="nav-link"
-                onClick={() => this.props.setPage(2)}
+                onClick={() => props.setPage(2)}
                 href="#"
               >
                 About Us
@@ -92,7 +41,7 @@ class NavBar extends Component {
             <li className="nav-item">
               <a
                 className="nav-link"
-                onClick={() => this.props.setPage(3)}
+                onClick={() => props.setPage(3)}
                 href="#"
               >
                 Our Idea
@@ -118,11 +67,6 @@ class NavBar extends Component {
         </button>
       </nav>
     );
-  }
-
-  // printOutPage() {
-  //   console.log("Prop page: ", this.props.page);
-  // }
-}
+};
 
 export default NavBar;
